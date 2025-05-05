00b949641212711de69d5e5deeda0ffce58a2f70
Lucas <borgesbfx@gmail.com>
2025-05-05 16:13:10 -0300

    feat(frontend): adding streamlit app to input the prompts

---
+++ backend/streamlit_app.py
@@ -0,0 +1,66 @@
+import os
+import requests
+import streamlit as st
+
+# Backend api url
+fastapi_url = "http://localhost:8000"
+
+# Input form
+st.title("🎴 Flyer Generator")
+text_prompt = st.text_area("Enter text prompt")
+uploaded_file = st.file_uploader("Upload an image (optional)", type=["jpg", "jpeg", "png"])
+
+with st.sidebar:
+    st.sidebar.header("Choose your model", divider="red")
+    model_name = st.selectbox(
+        "Select a model",
+        ("dall-e-3", "stable_diffusion", "flux"),
+        index=0,
+        help="Choose a model for generating text.",
+    )
+
+    st.sidebar.header("View generated flyers", divider="red")
+    col1, col2, col3 = st.sidebar.columns(3)
+
+    with col1:
+        st.image("./flyers_data/train/ENCARTE-1.jpg")
+        st.caption("ENCARTE-1")
+    
+    with col2:
+        st.image("./flyers_data/train/10042854.jpg")
+        st.caption("ENCARTE-2")
+    with col3:
+        st.image("./flyers_data/train/10557831.jpg")
+        st.caption("ENCARTE-3")
+
+
+if st.button("Generate Flyer"):
+    if not text_prompt and not uploaded_file:
+        st.error("Please enter text or upload an image.")
+    else:
+        # Prepare data for API request
+        prompt_data = {}
+        if text_prompt:
+            prompt_data['text'] = text_prompt
+        if uploaded_file:
+            os.makedirs("./temp_uploads", exist_ok=True)
+            temp_path = f"./temp_uploads/{uploaded_file.name}"
+            with open(temp_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            prompt_data["images"] = [temp_path]
+        
+        # Call backend endpoints
+        try:
+            response = requests.post(f"{fastapi_url}/api/generate_flyer", json=prompt_data)
+            if response.status_code == 200:
+                result = response.json()
+                st.success(result['message'])
+
+                # DIsplay generated flyer
+                flyer_path = result['path']
+                st.image(flyer_path, caption="Generated Flyer", use_column_width=True)
+            else:
+                st.error(f"Error: {response.json().get('detail')}")
+        
+        except Exception as e:
+                st.error(f"Error: {str(e)}")
